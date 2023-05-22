2d1fd9abf9beb2c3a3c46b416712486b2aa39890
ks-developer <ks.business.developer@gmail.com>
2023-05-22 18:01:15 +0530

    added README

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# logBrain
+## Log Parser
+
+The Log Parser is a Python class that allows you to parse log files based on a specified log format. It extracts relevant log entries from a log file and provides methods to retrieve specific fields from the log entries.
+
+**Features**
+
+ - Parse log files based on a specified log format
+   
+ - Extract IP addresses, timestamps, request details, response statuses,bytes sent, referer URLs, and user agents from log entries
+ - Generate a Pandas DataFrame from the parsed log entries
+ - Keep track of any log entries that failed to parse
+
+
+**Requirements**
+
+   - Python 3.x
+ - re (regular expression) module
+ - pandas library
+ 
+**Usage**
+
+    pip install -r requirements.txt
+
+
+## Instantiate the LogParser class:
+
+    log_parser = LogParser()
+
+Parse a log file by calling the parse_log method and providing the path to the log file as an argument. This method returns a list of dictionaries, where each dictionary represents a parsed log entry.
+
+    entries = log_parser.parse_log(logfile_path)
+
+Extract specific fields from the log entries using the provided methods, such as extract_ip, extract_time, extract_request, extract_status, extract_bytes_sent, extract_referer, and extract_user_agent.
+
+    ip = log_parser.extract_ip()
+    time = log_parser.extract_time()
+    request = log_parser.extract_request()
+    status = log_parser.extract_status()
+    bytes_sent = log_parser.extract_bytes_sent()
+    referer = log_parser.extract_referer()
+    user_agent = log_parser.extract_user_agent()
+
+  Generate a Pandas DataFrame from the parsed log entries using the to_df method.
+
+      df = log_parser.to_df(entries)
+
+
+Additional Notes
+
+The log file can be read using the read_log_file method, which returns the contents of the log file as a list of strings.
+
+The print_matches method can be used to display the matched fields and their values from a log entry.
+
+Feel free to customize the code and adapt it to your specific use case. Happy log parsing!
+
--- logparser.py
+++ logparser.py
@@ -1,27 +1,103 @@
+
 import re
-import shlex
 import pandas as pd
+from typing import List
+
+class LogParser:
+
+    def __init__(self) -> None:
+
+        self.log_format  = r'(?P<remote_addr>\d+\.\d+\.\d+\.\d+)\s+\S+\s+\S+\s+\[(?P<time>[^\]]+)\]\s+"(?P<request>[^"]+)"\s+(?P<status>\d+)\s+(?P<bytes_sent>\d+)\s+"(?P<referer>[^"]+)+"\s+"(?P<user_agent>(?!http)[^"]*)"'
+        self.fails = []
+
+    def search(self,regex,log_string):
+        return re.search(regex,log_string)
+    
+    def match(self,regex,log_string):
+        return re.match(regex,log_string)
 
 
-class Parser:
-    IP = 0
-    TIME = 3
-    TIME_ZONE = 4
-    REQUESTED_URL = 5
-    STATUS_CODE = 6
-    USER_AGENT = 9
+    def parse_log(self,logfile_path):
+        entries = []
+    
+        for log_string in self.read_log_file(logfile_path):
+            try:
+                self.result = self.search(self.log_format,log_string)
+                if self.result:
+                
+                    ip = self.extract_ip()
+                    time = self.extract_time()
+                    request = self.extract_request()
+                    status = self.extract_status()
+                    bytes_sent = self.extract_bytes_sent()
+                    referer =self.extract_referer()
+                    user_agent = self.extract_user_agent()
+                    entry = {
+                        "ip":ip,
+                        "time":time,
+                        "request":request,
+                        "status":status,
+                        "bytes_sent":bytes_sent,
+                        "referer":referer,
+                        "user_agent":user_agent       
+                    }
+                    entries.append(entry)      
+                
+            except Exception as e:
+                
+                print("log_string==>",log_string)
+                self.fails.append(log_string)
+ 
+        return entries
+    
+    def read_log_file(self,logfile_path:str)->List[str]:
+        contents = []
+        with open(logfile_path,'r') as f:
+            for line in f.readlines():
+                contents.append(line)
+        return contents
+        
+    def extract_ip(self):
 
-    def parse_line(self, line):
-        try:
-            line = re.sub(r"[\[\]]", "", line)
-            data = shlex.split(line)
-            result = {
-                "ip": data[self.IP],
-                "time": data[self.TIME],
-                "status_code": data[self.STATUS_CODE],
-                "requested_url": data[self.REQUESTED_URL],
-                "user_agent": data[self.USER_AGENT],
-            }
-            return result
-        except Exception as e:
-            raise e
+        return self.result.group("remote_addr") if self.result else None
+
+    
+    def extract_time(self):
+
+        return self.result.group("time") if self.result else None
+        
+    def extract_request(self):
+      
+        return self.result.group("request") if self.result else None
+        
+
+    def extract_status(self):
+
+        return self.result.group("status") if self.result else None
+        
+    
+    def extract_bytes_sent(self):
+
+        return self.result.group("bytes_sent") if self.result else None
+     
+    
+    def extract_referer(self):
+        
+        return self.result.group("referer") if self.result else None
+
+    
+    def extract_user_agent(self):
+        
+        return self.result.group("user_agent") if self.result else None
+
+
+    def print_matches(self,match):
+        for group_name,group_value in match.groupdict().items():
+            print(f"{group_name}:{group_value}")
+
+    def to_df(self,entries):
+        return pd.DataFrame(entries)
+    
+    def failures(self):
+        return self.fails
+
